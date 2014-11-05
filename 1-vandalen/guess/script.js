e5efcce0be52a88dade845745d71f23b81945536
ed222gg <ed222gg@student.lnu.se>
2014-11-05 13:57:25 +0000

    småfix med hemliga talet

--- 1-vandalen/guess/script.js
+++ 1-vandalen/guess/script.js
@@ -2,12 +2,40 @@
 
 window.onload = function(){
 	
-	var secret = 50; // Detta tal behöver bytas ut mot ett slumpat tal.
+	var secret = Math.floor(Math.random() * 100) + 1; // Detta tal behöver bytas ut mot ett slumpat tal.
+	var numberOfGuesses = 0;// antalet gissningar användaren har gjort
+	
+
 	
 	// I denna funktion ska du skriva koden för att hantera "spelet"
 	var guess = function(number){
 		console.log("Det hemliga talet: " + secret); // Du når den yttre variabeln secret innifrån funktionen.
 		console.log("Du gissade: " + number); // Detta nummer är det som användaren gissade på.
+		
+		
+		
+		numberOfGuesses += 1;
+		if (isNaN(number)) {
+			return [false, "Du gissade inte på ett tal. Gissa på ett tal mellan 1 och 100 "];
+			
+		}
+	
+		else if (number < 1 || number > 100) {
+            return [false, "Talet är utanför intervallet 0 - 100 "];
+		}
+            
+		else if (number < secret){
+			return [false, "Det hemliga talet är högre än " + number];
+		}
+		
+		else if (number > secret){
+			 return [false, "Det hemliga talet är lägre än " + number];
+			
+		}
+		else{
+			return [true, "Grattis du vann! Det hemliga talet var " +secret+ ", och du behövde " +numberOfGuesses+ " försök för att hitta det"];}
+	
+		
 			
 		// Plats för förändring.
 

81306b43fd3c16b5ce04af854c51ce2b7d5fda9e
ed222gg <ed222gg@student.lnu.se>
2014-12-04 17:49:54 +0000

    vänder tillbaka korten

--- 3-gameon/memory/script/Memory.js
+++ 3-gameon/memory/script/Memory.js
@@ -1,24 +1,64 @@
 "use strict"; //objekt.metod/egenskap(argument)
 
 var Memory = {
-     //array för textmeddelanden
-    randomImg: [],
-    memoryValue: [],
-    memoryTilesId: [],
-   
+                // 2,2,4,1,0,3,1,3,4
+    memoryArray: [],
+    imgTagArray: [],
+    count: 0,
     init: function() {
-    
-     Memory.randomImg.push(RandomGenerator.getPictureArray(3, 3));
-     console.log(Memory.randomImg);
-    Memory.memoryBoard(3, 3);
+        var board = document.getElementById("grid");
+
+        Memory.memoryArray.push(RandomGenerator.getPictureArray(4, 4));
+        console.log(Memory.memoryArray);
+        Memory.memoryBoard(4, 4, Memory.memoryArray);
+        // Memory.click(Memory.MemoryArray); 
+        
+        
+        var colClassList = document.querySelectorAll(".colClass"); // samlar alla element med classen colclass i en lista
+       
+       // board.addEventListener("click", function(){ Memory.click(colClassList); }); // skapar klickevent till board
+        
+        // Lopar längden av alla element i colClassen, kallar på eventclick, vi kallar på eventklick 
+        //för att jag inte få ha en funktion inuti en loop för då blir i alltid fel nummer.
+        for (var i = 0; i < colClassList.length; i++) {
+            eventClick(i);
+        }
+        // funktion som vi kallar på i forlopen ovan, den lägger en eventlistener på alla element i colClassList
+        function eventClick(locationId){
+             colClassList[locationId].addEventListener("click", function() {
+                Memory.count ++;
+                console.log(Memory.count);
+                        var counter = document.getElementById("counter");
+                        counter.innerHTML = "Antal klick: " + Memory.count;
+
+                Memory.click(Memory.memoryArray[0][locationId], locationId);// Kallar på memory.click och skickar med bildnummret från arrayen memoryArray samt locationId som är ett ökande nummer
+              //Memory.click(Memory.memoryArray[0][locationId],  Memory.imgTagArray[locationId]);
+            });
+        }
         
     },
-
-    imgArray: function() {
-    },
     
-    memoryBoard: function(rows, cols){
-        var grid =  document.getElementById("grid");
+    click: function(picId, locationId /*img*/){ // skickar med colclasslist till click funktion
+       
+       
+        Memory.imgTagArray[locationId].setAttribute("src", "pics/"+picId+".png"); // sätter nya bilder med bildnumret från picId på rätt plats i imgTagArrayen
+        //img.setAttribute("src", "pics/"+picId+".png")
+       
+         setTimeout(function(){
+        Memory.imgTagArray[locationId].setAttribute("src", "pics/0.png");
+         
+         }, 1000);
+       
+    },
+   //  timer: function(locationId){
+        //  Memory.imgTagArray[locationId].setAttribute("src", "pics/0.png");
+         
+   //  },
+   
+
+    memoryBoard: function(rows, cols, memoryArray){
+        var grid = document.getElementById("grid");
+        var cards = document.getElementById("cards");
         var i = undefined;
         var j = undefined;
         var row = undefined;
@@ -26,28 +66,39 @@ var Memory = {
         var img = undefined;
         var tiledflipped = 0;
         var output = '';
+        var a = undefined;
         
         for (i = 0; i < rows; i++){
             
             row = document.createElement("tr");  
             for (j = 0; j < cols ; j++) {
+                //skapar element
                 col = document.createElement("td");   
                 img = document.createElement("img");
+                a = document.createElement("a"); // skapar en a tagg
                 
+                Memory.imgTagArray.push(img); // lägger in (pushar in) img elementet i en array med namnet imgTagArray
+                //Sätter egenskaper
+                a.setAttribute("href", "#");
                 img.setAttribute("src", "pics/0.png");
-                col.setAttribute("class", "colClass");
-                
-                col.appendChild(img);
+                col.setAttribute("class", "colClass"); //mina kort
+           
+                //Lägger till dom till HTML-sidan
+                a.appendChild(img);
+                col.appendChild(a);
                 row.appendChild(col);
+                
             }
             row.setAttribute("class", "rowClass");
             grid.appendChild(row);
+           
         }
-      
-                
-        
+        //var counter = document.getElementById("counter");
+      //  counter.innerHTML = "Antal klick: " + Memory.count;
         
     },
+    
+    
     
 };
 
